e3f2c4458a976b5d59d9585afb120bc3ebde5083
Shiju John <shijujohn1093@gmail.com>
2023-08-16 16:23:53 +0000

    Added procedure scripts for learning and debussing

---
+++ adventure_procedure_sample/learning_procedure.sql
@@ -0,0 +1,35 @@
+﻿USE [AdventureWorks2022]
+GO
+ALTER PROCEDURE dbo.GetOrderTrackingForCustomer
+	@storeName varchar(50),
+	@trackingNumber varchar(25) OUTPUT
+AS
+BEGIN
+	-- Declare Variable
+	DECLARE @salesOrderId int;
+
+	
+	SELECT TOP 1 @salesOrderId = SOH.SalesOrderID
+	FROM Sales.SalesOrderHeader SOH
+	INNER JOIN Sales.Customer C ON SOH.CustomerID = C.CustomerID
+	INNER JOIN Sales.Store S ON S.BusinessEntityID = C.StoreID
+	WHERE 1=1
+	AND S.Name = @storeName
+	ORDER BY SOH.OrderDate DESC;
+
+	IF @@ROWCOUNT > 1 BEGIN
+		SELECT TOP 1 @trackingNumber = SOD.CarrierTrackingNumber
+		FROM Sales.SalesOrderDetail SOD
+		WHERE 1=1
+		AND SOD.SalesOrderDetailID = @salesOrderId 
+		AND SOD.CarrierTrackingNumber is not null;
+	END
+	ELSE BEGIN
+		SET @trackingNumber = 'Store Not Found'
+	END
+
+	PRINT @storeName + ' Tracking Number '+ @trackingNumber
+
+END
+
+
---
+++ adventure_procedure_sample/testing_procedure.sql
@@ -0,0 +1,21 @@
+﻿-----------------------------------
+--      Testing                 ---
+-----------------------------------
+-- >> Test 1
+DECLARE @name varchar(25)
+DECLARE @trackInfo varchar(50)
+
+SET @name = 'Good Toys';
+SET @trackInfo = null;
+
+exec dbo.GetOrderTrackingForCustomer @name, @trackInfo OUTPUT
+select 'Test 1', @trackInfo
+
+
+-- >> Test 2
+SET @name = 'Broken Toys';
+SET @trackInfo = null;
+
+exec dbo.GetOrderTrackingForCustomer @name, @trackInfo OUTPUT
+select 'Test 2', @trackInfo
+ 
